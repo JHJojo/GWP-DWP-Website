e0d6fb1c229d2801a9ab9239d1612d52b5dd4b80
jo5642li <jonas.liehmann@fh-erfurt.de>
2021-12-04 00:33:14 +0100

    rmd old navbar

--- navigation/navbar.php
+++ navigation/navbar.php
@@ -4,24 +4,8 @@
 
 <nav class="navbar">
     <div class="container">
-
         <div>
-            <ul>
-                <li><a href="#">
-                    <span class="icons"><img class="iconLogin" src="../images/person.svg"  alt="loginIcon"></span>
-                        Anmelden</a></li>
-                <li><a href="#">
-                    <span class="icons"><img class="iconWishlist" src="../images/herz.svg" alt="wishlistIcon"></span>
-                    Wunschzettel</a></li>
-                <li><a href="#">
-                    <span class="icons"><img class="iconFaq" src="../images/sprechblase.svg" alt="faqIcon"></span>
-                    FAQ / Hilfe</a></li>
-                <li><a href="#">
-                    <span class="icons"><img class="iconShoppingcart" src="../images/warenkorb.svg" alt="shoppingcartIcon"></span>
-                    Warenkorb</a></li>
-            </ul>
         </div>
-
     </div>
 </nav>
 
