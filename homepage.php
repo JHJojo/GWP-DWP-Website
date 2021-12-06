fc5c290b61f5260908befc2efae58831011ea397
JHJojo <70706428+JHJojo@users.noreply.github.com>
2021-12-06 19:01:46 +0100

    improved homepage

--- homepage.php
+++ homepage.php
@@ -1,10 +1,7 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/navigation/header.php';?>
 <?php
-$username = "gk-db";
-  $password = "123456789";
-  $dsn = "mysql:host=127.0.0.1;dbname=gk-db;charset=utf8";
-  $db = new PDO($dsn,$username,$password);
+  $db = new PDO("mysql:host=127.0.0.1;dbname=gk-db;charset=utf8","gk-db","123456789");
 
   $sql = "SELECT productID,description,price,photo
           FROM products";
@@ -13,6 +10,9 @@ $username = "gk-db";
   FROM products LIMIT 4");
   $result2 = $db->query("SELECT productID,description,price,photo
   FROM products ORDER BY productID DESC LIMIT 4");
+
+  $result3 = $db->query("SELECT productID,description,price,photo
+  FROM products ORDER BY productID DESC LIMIT 16");
 ?>
 
 <!-- HEAD -->
@@ -24,18 +24,32 @@ $username = "gk-db";
 <div class="container">
   <!-- CONTENT -->
   <div class="content homepage">
-    <h1 class="text-center">Highlights</h1>
+    <h1 class="text-center display-3">Highlights</h1>
+
+    <hr>
   
     <div class="fav-products">
       <div class="flex-row">
-       <?php while($row = $result->fetch()):?>
-        <?php include './card.php' ?>
-       <?php endwhile;?>
+        <?php while($row = $result->fetch()):?>
+          <div class="col-4">
+            <?php include './components/card.php' ?>
+          </div>
+        <?php endwhile;?>
       </div>
       <div class="flex-row">
-      <?php while($row = $result2->fetch()):?>
-        <?php include './card.php' ?>
-       <?php endwhile;?>
+        <?php while($row = $result2->fetch()):?>
+          <div class="col-4">
+            <?php include './components/card.php' ?>
+          </div>
+        <?php endwhile;?>
+      </div>
+<hr>
+      <div class="flex-row flex-wrap justify-space-between">
+        <?php while($row = $result3->fetch()):?>
+          <div class="col-4 my-12">
+            <?php include './components/card.php' ?>
+          </div>
+        <?php endwhile;?>
       </div>
     </div>
   </div>
