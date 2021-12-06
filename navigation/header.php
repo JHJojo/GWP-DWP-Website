8f098884862c4cac9a2efbe11f93929438082e71
jo5642li <jonas.liehmann@fh-erfurt.de>
2021-12-06 11:43:49 +0100

    rmd additional div

--- navigation/header.php
+++ navigation/header.php
@@ -3,51 +3,49 @@
 </head>
 
 <!-- CONTAINER -->
-<div>
-  <header class="header">
-    <div class="container">
-      <!-- CONTENT -->
-      <div class="ml-auto">
-        <ul class="links">
-          <li class="mr-2">
-            <a href="/login.php" class="link btn btn-primary btn-flat py-1">
-              <i class="material-icons md-18 mr-1">
-              person
-              </i>
-              Anmelden
-            </a>
-          </li>
-          <li class="mr-2">
-            <a href="#" class="link btn btn-primary btn-flat py-1">
-              <i class="material-icons md-18 mr-1">
-              favorite
-              </i>
-              Wunschzettel
-            </a>
-          </li>
-          <li class="mr-2">
-            <a href="#" class="link btn btn-primary btn-flat py-1">
-              <i class="material-icons md-18 mr-1">
-              question_answer
-              </i>
-              FAQ / Hilfe
-            </a>
-          </li>
-          <li>
-            <a href="#" class="link btn btn-primary btn-flat py-1">
-              <i class="material-icons md-18 mr-1">
-              shopping_cart
-              </i>
-              Warenkorb
-            </a>
-          </li>
-        </ul>
-      </div>
+<header class="header">
+  <div class="container">
+    <!-- CONTENT -->
+    <div class="ml-auto">
+      <ul class="links">
+        <li class="mr-2">
+          <a href="/login.php" class="link btn btn-primary btn-flat py-1">
+            <i class="material-icons md-18 mr-1">
+            person
+            </i>
+            Anmelden
+          </a>
+        </li>
+        <li class="mr-2">
+          <a href="#" class="link btn btn-primary btn-flat py-1">
+            <i class="material-icons md-18 mr-1">
+            favorite
+            </i>
+            Wunschzettel
+          </a>
+        </li>
+        <li class="mr-2">
+          <a href="#" class="link btn btn-primary btn-flat py-1">
+            <i class="material-icons md-18 mr-1">
+            question_answer
+            </i>
+            FAQ / Hilfe
+          </a>
+        </li>
+        <li>
+          <a href="#" class="link btn btn-primary btn-flat py-1">
+            <i class="material-icons md-18 mr-1">
+            shopping_cart
+            </i>
+            Warenkorb
+          </a>
+        </li>
+      </ul>
     </div>
-  </header>
-  
-  <?php include $_SERVER['DOCUMENT_ROOT'] . '/navigation/navbar.php';?>
-</div>
+  </div>
+</header>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/navigation/navbar.php';?>
 
 
 <script>
