7e586fe85c844a11ff64e75e863be74f1b1c51b0
JHJojo <70706428+JHJojo@users.noreply.github.com>
2021-12-06 18:59:16 +0100

    improved navbar

--- navigation/navbar.php
+++ navigation/navbar.php
@@ -10,24 +10,54 @@
         <div class="container">
             <!-- CONTENT -->
             <div class="top-navigation">
-
                 <div class="my-banner py-2">
-                    <div class="side-info d-flex">
-                        <img src="https://i.imgur.com/lZcL1h8.png" alt="banner">
-                        <p class="ml-auto">
-                            <i class="material-icons md-52 mr-2">
-                                verified_user
-                            </i>
-                        </p>
-                        <p class="mr-10">1 Monat <br> Erfahrung</p>
-                        <i class="material-icons md-52 mr-2">
-                            devices
-                        </i>
-                        <p class="mr-10">LOW-END <br> Hardware</p>
-                        <i class="material-icons md-52 mr-2">
-                            local_shipping
-                        </i>
-                        <p class="mr-2">KEIN <br> Versand!</p>
+                    <div class="flex-row no-gutters align-center">
+                        <div class="col-3">
+                           <a href="../homepage.php">
+                               <img src="https://i.imgur.com/lZcL1h8.png" alt="banner">
+                            </a> 
+                        </div>
+                        
+                        <div class="flex-spacer flex-grow"></div>
+
+                        <div class="flex-shrink">
+                            <div class="flex-row">
+                                <div class="flex-shrink">
+                                    <i class="material-icons md-52 mr-2">
+                                        verified_user
+                                    </i>
+                                </div>
+                                <div class="flex-grow space-nowrap">
+                                    1 Monat<br>Erfahrung
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="flex-shrink mx-4">
+                            <div class="flex-row">
+                                <div class="flex-shrink">
+                                    <i class="material-icons md-52 mr-2">
+                                    devices
+                                    </i>
+                                </div>
+                                <div class="flex-grow space-nowrap">
+                                    LOW-END<br>Hardware
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="flex-shrink">
+                            <div class="flex-row">
+                                <div class="flex-shrink">
+                                    <i class="material-icons md-52 mr-2">
+                                        local_shipping
+                                    </i>
+                                </div>
+                                <div class="flex-grow space-nowrap">
+                                    KEIN <br> Versand!
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -35,36 +65,36 @@
     </div>
 
     <div class="container">
-        <div class="cat-buttons d-flex">
-            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 mx-auto">
+        <div class="cat-buttons flex-row">
+            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 flex-grow ">
                 Prozessoren
             </a>
-            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 mx-auto">
+            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 flex-grow">
                 Grafikkarten
             </a>
-            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 mx-auto">
+            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 flex-grow">
                 Arbeitsspeicher
             </a>
-            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 mx-auto">
+            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 flex-grow">
                 Gehäuse
             </a>
-            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 mx-auto">
+            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 flex-grow">
                 Netzteile
             </a>
 
-            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 mx-auto">
+            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 flex-grow">
                 Monitore
             </a>
-            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 mx-auto">
+            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 flex-grow">
                 Festplatten
             </a>
-            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 mx-auto">
+            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 flex-grow">
                 Mainboards
             </a>
-            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 mx-auto">
+            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 flex-grow">
                 Kühlung
             </a>
-            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 mx-auto">
+            <a href="#" class="btn btn-outlined btn-flat py-4 pl-3 flex-grow">
                 Andere
             </a>
         </div>
