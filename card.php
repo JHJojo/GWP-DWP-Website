c891f78a0bce29f2e7f50532081b1952615045ec
JHJojo <70706428+JHJojo@users.noreply.github.com>
2021-12-06 15:39:07 +0100

    renamed stuff

--- card.php
+++ card.php
@@ -2,7 +2,7 @@
 <link href="/styles/image.css" rel="stylesheet" type="text/css" />
 
 <div class="card flex-column"> 
-    <img class="image-startingpage" src="<?= $row['photo'] ?>" alt="image">
+    <img class="image-homepage" src="<?= $row['photo'] ?>" alt="image">
     <div class="card-body">
         <?= $row['description'] ?>
     </div>
