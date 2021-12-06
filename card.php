43b53a6ce9b0019dc72e9ae5fabdd24e1fe318d4
JHJojo <70706428+JHJojo@users.noreply.github.com>
2021-12-06 15:35:06 +0100

    added card template

---
+++ card.php
@@ -0,0 +1,17 @@
+
+<link href="/styles/image.css" rel="stylesheet" type="text/css" />
+
+<div class="card flex-column"> 
+    <img class="image-startingpage" src="<?= $row['photo'] ?>" alt="image">
+    <div class="card-body">
+        <?= $row['description'] ?>
+    </div>
+    <div class="card-footer d-flex">
+            <a href="#" class="btn btn-info btn-flat">
+                Details
+            </a>
+            <a href="#" class="btn btn-accent btn-flat">
+                Warenkorb
+            </a>
+    </div>
+</div>
