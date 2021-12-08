104b934ffe0c6308144ffa8dd792bd43347e8d4b
JHJojo <70706428+JHJojo@users.noreply.github.com>
2021-12-08 12:08:44 +0100

    added deliverytype page

---
+++ delivery.php
@@ -0,0 +1,69 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/navigation/header.php';?>
+
+<!-- HEAD -->
+<head>
+<link href="/styles/help-pages.css" rel="stylesheet" type="text/css" />
+</head>
+
+<!-- CONTAINER -->
+<div class="container">
+  <!-- CONTENT -->
+  <div class="content payment">
+    <h1 class="display-1">Abholung & Versand</h1>
+
+    <div class="types flex-column">
+      <div class="type1 my-2">
+        <h2 class="headline text-center">Espresso Versand</h2>
+        <div class="flex-row">
+          <div class="col-4 my-auto">
+              <img class=""src="https://i.imgur.com/0IUPRMs.jpg" alt="espresso-versand">
+          </div>
+          <div class="col-8">
+            <p>
+             Von Postboten aus Mailand und Makatao in Kooperation entwickelt, damit sie Ihre Sendung auf Wunsch cremig / schwarz / extra stark erreicht. <br><br> 
+             Versandkosten je Bestellung: 7 g Kaffee
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="type2 my-2">
+        <h2 class="headline text-center">EH Express – Einhorn Express</h2>
+        <div class="flex-row">
+          <div class="col-4 my-auto">
+              <img class=""src="https://i.imgur.com/EOIqltS.png" alt="einhorn-express">
+          </div>
+          <div class="col-8">
+            <p>
+             Unser Versand-Partner ist EH – Express: Der Lieferdienst arbeitet mit zuverlässigen und schnellen Einhörnern, die artgerecht gehalten werden. 
+             Gegen einen Aufpreis kann der Einhorntransport auch CO2-Emissions-reduziert erfolgen. <br><br> 
+             Versandkosten je Bestellung: 2 kg Mören
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="type3 my-2">
+        <h2 class="headline text-center">Abholung</h2>
+        <div class="flex-row">
+          <div class="col-4 my-auto">
+              <img class=""src="https://i.imgur.com/mXKbEJC.png" alt="einhorn-express">
+          </div>
+          <div class="col-8">
+            <p>
+             Aufgrund der aktuellen Corona-Lage hat sich unser Team von der Außenwelt abgeschnitten. <br><br>
+             DERZEIT NICHT MÖGLICH!
+            </p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+<script>
+</script>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/navigation/footer.php'?>
