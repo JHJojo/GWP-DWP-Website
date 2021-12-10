88cc5b8396aac6a4afdd5199111731c5bbc6b06b
JHJojo <70706428+JHJojo@users.noreply.github.com>
2021-12-10 15:34:50 +0100

    removed unused code

--- homepage.php
+++ homepage.php
@@ -1,20 +1,15 @@
+
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/navigation/header.php';?>
-<?php
-  $db = new PDO("mysql:host=127.0.0.1;dbname=gk-db;charset=utf8","root","");
-
+<?php 
+  error_reporting(-1);
+  ini_set('display_errors', 'On');
+ 
   $sql = "SELECT productID,description,price,photo
-          FROM products";
-
-  $result = $db->query("SELECT productID,description,price,photo
-  FROM products LIMIT 4");
-  $result2 = $db->query("SELECT productID,description,price,photo
-  FROM products ORDER BY productID DESC LIMIT 4");
-
-  $result3 = $db->query("SELECT productID,description,price,photo
-  FROM products ORDER BY productID DESC LIMIT 16");
+           FROM products LIMIT 9";
+  
+  $result = getDB()->query($sql);
 ?>
-
 <!-- HEAD -->
 <head>
   <link href="/styles/homepage.css" rel="stylesheet" type="text/css" />
@@ -27,32 +22,15 @@
     <h1 class="text-center display-3">Highlights</h1>
 
 <hr>
-  
    <div class="fav-products">
-   <!--    <div class="flex-row">
-        <?php while($row = $result->fetch()):?>
-          <div class="col-4">
-            <?php include './components/card.php' ?>
-          </div>
-        <?php endwhile;?>
-      </div>
-      <div class="flex-row">
-        <?php while($row = $result2->fetch()):?>
-          <div class="col-4">
-            <?php include './components/card.php' ?>
-          </div>
-        <?php endwhile;?>
-      </div>    
-<hr>--> 
       <div class="flex-row flex-wrap justify-space-between">
-        <?php while($row = $result3->fetch()):?>
+        <?php while($row = $result->fetch()):?>
           <div class="col-4 my-12">
             <?php include './components/card.php' ?>
           </div>
         <?php endwhile;?>
       </div>
     </div>
-
 
 <hr>
     <h2 class="headline text-center">Entdecke Gehäusekönig</h2>
