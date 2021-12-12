e36534d1cccc5a5868b9c266c8c1e2022d7e0446
jo5642li <jonas.liehmann@fh-erfurt.de>
2021-12-12 17:02:05 +0100

    fixed index, not including cards

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+  $db = new PDO("mysql:host=127.0.0.1;dbname=gk-db;charset=utf8","root","");
+
+  $sql = "SELECT productID,description,price,photo
+          FROM products";
+
+  $result = $db->query("SELECT productID,description,price,photo
+  FROM products LIMIT 4");
+  $result2 = $db->query("SELECT productID,description,price,photo
+  FROM products ORDER BY productID DESC LIMIT 4");
+
+  $result3 = $db->query("SELECT productID,description,price,photo
+  FROM products ORDER BY productID DESC LIMIT 16");
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -16,32 +31,39 @@
     <!-- CONTAINER -->
     <div class="container">
       <!-- CONTENT of my-page -->
-      <div class="content my-page">
-        <h1>Index</h1>
-        <a class="btn btn-accent" href="/views/showcase.php">
-          To the Showcase
-          <span class="material-icons-outlined">
-            north_east
-          </span>
-        </a>
-        <a class="btn btn-info" href="/views/imprint.php">
-          Impressum
-          <span class="material-icons-outlined">
-            north_east
-          </span>
-        </a>
-        <a class="btn btn-secondary" href="/views/about-us.php">
-        about-us
-          <span class="material-icons-outlined">
-            north_east
-          </span>
-        </a>
-        <a class="btn btn-secondary" href="/views/homepage.php">
-        homepage
-          <span class="material-icons-outlined">
-            north_east
-          </span>
-        </a>
+      <div class="content homepage">
+        <h1 class="text-center display-3">Highlights</h1>
+
+        <hr>
+      
+        <div class="fav-products">
+        <!--    <div class="flex-row">
+            <?php while($row = $result->fetch()):?>
+              <div class="col-4">
+                <?php include './components/card.php' ?>
+              </div>
+            <?php endwhile;?>
+          </div>
+          <div class="flex-row">
+            <?php while($row = $result2->fetch()):?>
+              <div class="col-4">
+                <?php include './components/card.php' ?>
+              </div>
+            <?php endwhile;?>
+          </div>    
+        <hr>--> 
+          <div class="flex-row flex-wrap justify-space-between">
+            <?php while($row = $result3->fetch()):?>
+              <div class="col-4 my-12">
+                <?php include './components/card.php' ?>
+              </div>
+            <?php endwhile;?>
+          </div>
+        </div>
+        
+        <hr>
+
+        <h2 class="headline text-center">Entdecke Gehäusekönig</h2>
       </div>
     </div>
 
--- navigation/footer.php
+++ navigation/footer.php
@@ -1,5 +1,5 @@
 <!-- move footer to bottom -->
-<div class="container flex-spacer white"></div> 
+<div class="container flex-spacer white"></div>
 
 <!-- CONTAINER -->
 <footer class="footer">
@@ -7,6 +7,31 @@
     <!-- CONTENT -->
     <div>
       <h5>Footer</h5>
+
+      <a class="btn btn-accent" href="/views/showcase.php">
+        To the Showcase
+        <span class="material-icons-outlined">
+          north_east
+        </span>
+      </a>
+      <a class="btn btn-info" href="/views/imprint.php">
+        Impressum
+        <span class="material-icons-outlined">
+          north_east
+        </span>
+      </a>
+      <a class="btn btn-secondary" href="/views/about-us.php">
+        about-us
+        <span class="material-icons-outlined">
+          north_east
+        </span>
+      </a>
+      <a class="btn btn-secondary" href="/views/homepage.php">
+        homepage
+        <span class="material-icons-outlined">
+          north_east
+        </span>
+      </a>
     </div>
   </div>
 </footer>
