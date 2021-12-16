fb9035f273d772dc33c6a44f3e1db0e499724fa8
jo5642li <jonas.liehmann@fh-erfurt.de>
2021-12-16 18:03:49 +0100

    main.css now loaded at last, to override flexbox

--- config/config.php
+++ config/config.php
@@ -5,8 +5,8 @@
 <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
-<link href="/styles/main.css" rel="stylesheet" type="text/css" />
 <link href="/styles/flex.css" rel="stylesheet" type="text/css" />
+<link href="/styles/main.css" rel="stylesheet" type="text/css" />
 
 <!-- header/footer -->
 <link href="/styles/footer.css" rel="stylesheet" type="text/css" />
