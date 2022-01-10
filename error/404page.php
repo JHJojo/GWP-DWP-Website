86dd65df0f670603c1507df9ddc4d7e8a2e3f000
jo5642li <jonas.liehmann@fh-erfurt.de>
2022-01-11 00:55:52 +0100

    added custom 404 error page and apache config

---
+++ error/404page.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';?>
+
+    <!-- HEAD -->
+    <title>PC Systeme & Komponenten online kaufen | Gehäusekönig</title>
+  </head>
+
+  <body>
+    <noscript>
+      Ihr Browser unterstützt kein Javascript oder legen Sie die Berechtigung dafür fest!
+    </noscript>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/navigation/header.php';?>
+
+    <!-- CONTAINER -->
+    <div class="container">
+      <!-- CONTENT of my-page -->
+      <div class="content my-page text-center">
+        <hr>
+        <h1 class="display-2">404 - Seite wurde nicht gefunden</h1>
+        <hr>
+        <div class="flex-row justify-center">
+          <figure class="col-6 ma-0">
+            <img src="https://i.ibb.co/w7CGBGj/page-lost.png" alt="Nicht gefunden Bild">
+
+            <figcaption>
+              <a href="/" class="btn btn-primary">
+                <span class="material-icons mr-2">
+                home
+                </span>
+                STARTSEITE
+              </a>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+    </div>
+
+    <!-- Script for my-page -->
+    <script type="text/javascript">
+      "use strict";
+    </script>
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/navigation/footer.php'?>
+  </body>
+</html>
