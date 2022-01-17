33e17bdd6caee90156294bc449336f571a46e57f
an6748le <andreas.lemke@fh-erfurt.de>
2022-01-17 14:53:08 +0100

    Added blocked submit button through JS

--- views/login.php
+++ views/login.php
@@ -27,27 +27,30 @@ include $_SERVER['DOCUMENT_ROOT'] . '/navigation/header.php';
                 <div class="login-row">
                     <div class="input-group">
                         <label>E-Mail-Adresse</label>
-                        <input type="email" size="40" maxlength="250" name="email">
+                        <input id="email" type="email" size="40" maxlength="250" name="email" required="required">
                     </div>
                 </div>
                 <div class="login-row">
                     <div class="input-group">
                         <label>Passwort</label>
-                        <input type="password" size="40" maxlength="250" name="password">
+                        <input id="password" type="password" size="40" maxlength="250" name="password" required="required">
                     </div>
                 </div>
                 <div id="login-error"></div>
             </div>
+
             <div class="login-row">
                 <div class="input-group">
-                    <button class="link btn btn-primary btn-flat py-1" type="submit">Anmelden</button>
+                    <button id="login-btn" class="btn" type="submit" disabled="disabled">Anmelden</button>
                 </div>
-            </div>
-            <div class="login-row">
                 <div class="input-group">
-                    <a class="link btn btn-primary btn-flat py-1" href="/views/create_account.php">Registrieren</a>
+                    <a id="register" class="btn" href="/views/create_account.php">Registrieren</a>
                 </div>
             </div>
+
+
+
+            <div id="#error"></div>
         </form>
     </div>
 </div>
@@ -75,21 +78,41 @@ include $_SERVER['DOCUMENT_ROOT'] . '/navigation/header.php';
             })
             .then((result) => result.text())
             .then((html) => {
-                console.log("html");
-                console.info("Ausgabe HTML: ", html);
                 if (html == "success") {
                     window.location.href = 'http://localhost/views/intern.php';
                 } else {
                     document.querySelector("#login-error").innerHTML = html;
                 }
-
-
             })
             .catch((err) => {
                 console.info("Error Output: ", err)
                 //document.querySelector("#content").innerHTML = html;
             });
     }
+
+
+
+    const submitBtn = document.getElementById('login-btn');
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+
+    function updateSubmitBtn() {
+        const emailValue = email.value.trim();
+        const passwordValue = password.value.trim();
+        console.log("email", emailValue);
+        console.log("password", passwordValue);
+        if (!((emailValue && passwordValue) == "")) {
+            console.log("email", email);
+            console.log("password", password);
+            submitBtn.removeAttribute('disabled');
+            submitBtn.style.background = "#005a99";
+        } else {
+            submitBtn.setAttribute('disabled', 'disabled');
+        }
+    }
+    email.addEventListener('keyup', updateSubmitBtn);
+    password.addEventListener('keyup', updateSubmitBtn);
 </script>
 </body>
 
