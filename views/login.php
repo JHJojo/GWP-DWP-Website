4e00f5432862d7d5a73580e5b96905065078a3bc
an6748le <andreas.lemke@fh-erfurt.de>
2022-01-19 21:44:51 +0100

    Added Noscript

--- views/login.php
+++ views/login.php
@@ -1,11 +1,8 @@
-<?php require  $_SERVER['DOCUMENT_ROOT'] . '/functions/loginValidation.php'; ?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/config/config.php'; ?>
-
     <!-- HEAD -->
     <link href="/styles/login.css" rel="stylesheet" type="text/css" />
     <title>PC Systeme & Komponenten online kaufen | Gehäusekönig</title>
@@ -45,7 +42,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/navigation/header.php';
 
             <div class="login-row">
                 <div class="input-group">
-                    <button id="login-btn" class="btn" type="submit" disabled="disabled">Anmelden</button>
+                    <button id="login-btn" class="btn" type="submit">Anmelden</button>
                 </div>
                 <div class="input-group">
                     <a id="register" class="btn" href="/views/create_account.php">Registrieren</a>
@@ -55,7 +52,36 @@ include $_SERVER['DOCUMENT_ROOT'] . '/navigation/header.php';
         </form>
     </div>
 </div>
+<noscript>
 
+    <?php
+    //session_start();
+    require  $_SERVER['DOCUMENT_ROOT'] . '/functions/database.php';
+    //Check if there is an account to login
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        if (isset($_POST['email'])) {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $email = strtolower($email);
+            //Get the data of the DB
+            $statement = getDB()->prepare("SELECT * FROM accounts WHERE email = :email");
+            $result = $statement->execute(array('email' => $email));
+            $account = $statement->fetch();
+
+            //Parse the password
+            if ($account !== false && password_verify($password, $account['passwordHash'])) {
+                $_SESSION['userid'] = $account['accountID'];
+                $_SESSION['rank'] = $account['rank'];
+                echo '<meta http-equiv="refresh" content="2; URL=../index.php">';
+                die();
+            } else {
+                $errorMessage = "<div class='container'><div class='mx-auto'>Die E-Mail-Adresse oder das Passwort waren ungültig! </div></div>";
+                echo $errorMessage;
+            }
+        }
+    }
+    ?>
+</noscript>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/navigation/footer.php' ?>
 <script>
     document
@@ -116,6 +142,9 @@ include $_SERVER['DOCUMENT_ROOT'] . '/navigation/header.php';
     email.addEventListener('keyup', updateSubmitBtn);
     password.addEventListener('keyup', updateSubmitBtn);
 </script>
+
+
+
 </body>
 
 </html>
