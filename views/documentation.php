9d451bed2d69b475e1fcf844dca439f96d8f5032
JHJojo <70706428+JHJojo@users.noreply.github.com>
2022-01-19 15:22:45 +0100

    start of documentaion

---
+++ views/documentation.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';?>
+
+    <!-- HEAD -->
+    <title>PC Systeme & Komponenten online kaufen | Gehäusekönig</title>
+  </head>
+
+  <body>
+    <noscript>
+      Ihr Browser unterstützt kein Javascript oder legen Sie die Berechtigung dafür fest!
+    </noscript>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/navigation/header.php';?>
+
+    <!-- CONTAINER -->
+    <div class="container">
+      <!-- CONTENT of my-page -->
+      <div class="content documentation">
+        <h1>Dokumentation</h1>
+        <a href="#kunde"><h2>1. Kunde</h2></a>
+        <a href="#nutzer"><h2>2. Nutzer</h2></a>
+        <a href="#recherche"><h2>3. Recherche</h2></a>
+        <a href="#layout"><h2>4. Seitenlayout</h2></a>
+        <a href="#design"><h2>5. Design</h2></a>
+
+        <hr>
+
+        <a name="kunde"><h2>1. Kunde</h2></a>
+        <ul>
+            <li>Gehäusekönig ist ein Online Computershop</li>
+            <li>mögliche Kunden wären lokale PC-Läden, welche ihre Reichweite durch Onlineverkauf vergrößern wollen</li>
+            <li>Neueinsteiger welche in die Branche einsteigen wollen</li>
+        </ul>
+
+        <a name="nutzer"><h2>2. Nutzer</h2></a>
+        <ul>
+            <li>die Website ist vor allem für Kunden im privaten Bereich optimiert</li>
+            <li>Möglichkeit wichtige Komponenten eines Computers als auch Zubehör zu bestellen</li>
+            <li>vorrangig Erwachsene aber auch Jugendliche</li>
+            <li>wichtige Details und Spezifikationen werden hervorgehoben damit sich auch Leien einfacher zurechtfinden</li>
+        </ul>
+
+        <a name="recherche"><h2>3. Recherche</h2></a>
+        <ul>
+            Wir haben uns an folgenden Seiten orientiert:
+            <li><a href="https://www.caseking.de/">1. CaseKing</a></li>
+            <br>
+            <img src="https://i.imgur.com/uXlbQYO.png" ><br>
+            <li><a href="https://www.mindfactory.de/">2. Mindfactory</a></li>
+            <br>
+            <img src="https://i.imgur.com/nPKHEIP.png" ><br>
+            <li><a href="https://www.alternate.de/">3. Alternate</a></li>
+            <br>
+            <img src="https://i.imgur.com/Bp1fHMK.png" ><br>
+        </ul>
+
+        <a name="layout"><h2>4. Seitenlayout</h2></a>
+        <ul>
+            <li>WireFrames:</li>
+            <img src="https://i.imgur.com/T1dGhNA.png">
+            <img src="https://i.imgur.com/kUxKDmF.png">
+            <li>SiteMap:</li>
+            <img src="https://i.imgur.com/JDLGhSY.png">
+        </ul>
+
+        <a name="design"><h2>5. Design</h2></a>
+    <ul>
+        <li>
+            Layout: <br> - Wir haben uns beim Layout an verschiedenen Online-Shops orientiert und nach unseren Vorlieben die besten Layouts kombiniert.
+            <br> - Dabei war uns ein simpler Aufbau wichtig, um die Navigation und Nutzung der Seite so einfach wie möglich zu gestalten.
+            <br> - Des Weiteren ist unsere Seite bis zu einer Auflösung von 965px in der Breite responsiv optimiert.
+        </li>
+        <li>
+            Farben: <br> - Wir nutzen Grau für den Hintergrund und hauptsächlich Blau aus Primärfarbe, da diese eine gewisse Ruhe und Neutralität ausstrahlt.
+            <br> - Wichtige Funktionen wie Buttons wurden dabei passend zu den Aktionen eingefärbt, damit sie dem Nutzer einfacher ins Auge fallen.
+        </li>
+        <li>
+            Schrift: <br> - Bei der Schriftart ("Orbitron") war es uns wichtig, dass diese gut lesbar aber auch passend zum Thema Computer/Hardware ist.
+            <br> - An geeigneten Stellen haben wir uns für die Nutzung von Icons entschieden, welche die Navigation nochmals erleichtern sollen.
+        </li>
+        <li>
+            Bilder: <br> - Bilder welche auf der Seite verwendet werden sind passend zum Thema gewählt und möglichst aussagekräftig.
+            <br> - Produkbilder sollten immer nur das Produkt selbst zeigen.
+        </li>
+    </ul>
+
+      </div>
+    </div>
+
+    <!-- Script for my-page -->
+    <script type="text/javascript">
+      "use strict";
+    </script>
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/navigation/footer.php'?>
+  </body>
+</html>
