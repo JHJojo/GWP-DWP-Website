a2227718cd26af370f5af958416cb4dd48ec05bf
JHJojo <70706428+JHJojo@users.noreply.github.com>
2022-01-20 16:04:55 +0100

    styled order succes page

--- views/order-success.php
+++ views/order-success.php
@@ -16,8 +16,24 @@
     <!-- CONTAINER -->
     <div class="container">
       <!-- CONTENT of my-page -->
-      <div class="content order-success">
-        <h1 class="text-center">Vielen Dank für ihre Bestellung bei GEHÄUSEKÖNIG</h1>
+      <div class="content order-success text-center">
+        <h1 class="mt-2">Vielen Dank für ihre Bestellung bei GEHÄUSEKÖNIG</h1>
+
+        <div class="flex-row justify-center">
+          <figure class="col-6 ma-0">
+            <i class="material-icons mb-10" style="color:#005a99;font-size:150px;">
+            check_circle_outline
+            </i>
+            <figcaption>
+              <a href="/" class="btn btn-primary">
+                <span class="material-icons mr-2">
+                home
+                </span>
+                STARTSEITE
+              </a>
+            </figcaption>
+          </figure>
+        </div>
       </div>
     </div>
 
