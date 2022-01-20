2c6f46a27f944389241b4eeb9c5c26bdf848c331
JHJojo <70706428+JHJojo@users.noreply.github.com>
2022-01-20 16:04:42 +0100

    documentation

--- views/documentation.php
+++ views/documentation.php
@@ -23,6 +23,11 @@
         <a href="#recherche"><h2>3. Recherche</h2></a>
         <a href="#layout"><h2>4. Seitenlayout</h2></a>
         <a href="#design"><h2>5. Design</h2></a>
+        <a href="#funktionalitäten"><h2>6. Funktionalitäten</h2></a>
+        <a href="#ermodell"><h2>7. ER-Modell</h2></a>
+        <a href="#rollenmodell"><h2>8. Rollenmodell</h2></a>
+        <a href="#flussbild"><h2>9. Flussbild</h2></a>
+        <a href="#reflektion"><h2>10. Reflektion</h2></a>
 
         <hr>
 
@@ -65,25 +70,92 @@
         </ul>
 
         <a name="design"><h2>5. Design</h2></a>
-    <ul>
-        <li>
-            Layout: <br> - Wir haben uns beim Layout an verschiedenen Online-Shops orientiert und nach unseren Vorlieben die besten Layouts kombiniert.
-            <br> - Dabei war uns ein simpler Aufbau wichtig, um die Navigation und Nutzung der Seite so einfach wie möglich zu gestalten.
-            <br> - Des Weiteren ist unsere Seite bis zu einer Auflösung von 965px in der Breite responsiv optimiert.
-        </li>
-        <li>
-            Farben: <br> - Wir nutzen Grau für den Hintergrund und hauptsächlich Blau aus Primärfarbe, da diese eine gewisse Ruhe und Neutralität ausstrahlt.
-            <br> - Wichtige Funktionen wie Buttons wurden dabei passend zu den Aktionen eingefärbt, damit sie dem Nutzer einfacher ins Auge fallen.
-        </li>
-        <li>
-            Schrift: <br> - Bei der Schriftart ("Orbitron") war es uns wichtig, dass diese gut lesbar aber auch passend zum Thema Computer/Hardware ist.
-            <br> - An geeigneten Stellen haben wir uns für die Nutzung von Icons entschieden, welche die Navigation nochmals erleichtern sollen.
-        </li>
-        <li>
-            Bilder: <br> - Bilder welche auf der Seite verwendet werden sind passend zum Thema gewählt und möglichst aussagekräftig.
-            <br> - Produkbilder sollten immer nur das Produkt selbst zeigen.
-        </li>
-    </ul>
+        <ul>
+            <li>
+                Layout: <br> - Wir haben uns beim Layout an verschiedenen Online-Shops orientiert und nach unseren Vorlieben die besten Layouts kombiniert.
+                <br> - Dabei war uns ein simpler Aufbau wichtig, um die Navigation und Nutzung der Seite so einfach wie möglich zu gestalten.
+                <br> - Des Weiteren ist unsere Seite bis zu einer Auflösung von 965px in der Breite responsiv optimiert.
+            </li>
+            <li>
+                Farben: <br> - Wir nutzen Grau für den Hintergrund und hauptsächlich Blau aus Primärfarbe, da diese eine gewisse Ruhe und Neutralität ausstrahlt.
+                <br> - Wichtige Funktionen wie Buttons wurden dabei passend zu den Aktionen eingefärbt, damit sie dem Nutzer einfacher ins Auge fallen.
+            </li>
+            <li>
+                Schrift: <br> - Bei der Schriftart ("Orbitron") war es uns wichtig, dass diese gut lesbar aber auch passend zum Thema Computer/Hardware ist.
+                <br> - An geeigneten Stellen haben wir uns für die Nutzung von Icons entschieden, welche die Navigation nochmals erleichtern sollen.
+            </li>
+            <li>
+                Bilder: <br> - Bilder welche auf der Seite verwendet werden sind passend zum Thema gewählt und möglichst aussagekräftig.
+                <br> - Produkbilder sollten immer nur das Produkt selbst zeigen.
+            </li>
+        </ul>
+
+        <a name="funktionalitäten"><h2>5. Design</h2></a>
+        <ul>
+          <li>Landingpage:</li>
+          <img src="https://i.imgur.com/ChZtavC.png">
+          <li>Datenschutz:</li>
+          <img src="https://i.imgur.com/TC2pFfh.png">
+          <li>Produkt-Details:</li>
+          <img src="https://i.imgur.com/dNs0CSe.png">
+          <li>Produktsuche:</li>
+          <img src="https://i.imgur.com/ucFJ1JN.png">
+          <li>404-Page:</li>
+          <img src="https://i.imgur.com/DGReyeP.png">
+          <li>About Us:</li>
+          <img src="https://i.imgur.com/EW61fUU.png">
+          <li>FAQ und Hilfe:</li>
+          <img src="https://i.imgur.com/DYvgtoI.png">
+          <li>Impressum:</li>
+          <img src="https://i.imgur.com/8BTLc8W.png">
+          <li>Wunschzettel:</li>
+          <img src="https://i.imgur.com/nYeX9oL.png">
+          <li>Warenkorb:</li>
+          <img src="https://i.imgur.com/OzSLBWC.png">
+          <img src="https://i.imgur.com/ht0qzJV.png">
+          <li>Lieferadresse:</li>
+          <img src="https://i.imgur.com/f2kRbqO.png">
+          <li>Checkout:</li>
+          <img src="https://i.imgur.com/KRT9KXI.png">
+          <li>Bestellung erfolgreich:</li>
+          <img src="https://i.imgur.com/MgRAmBV.png">
+        </ul>
+
+        <a name="ermodell"><h2>7. ER-Modell</h2></a>
+        <ul>
+            <li>ER-Modell / Relationales Modell:</li>
+            <img src="https://i.imgur.com/GGvM8qV.png">
+        </ul>
+
+        <a name="rollenmodell"><h2>8. Rollenmodell</h2></a>
+
+        <a name="flussbild"><h2>9. Flussbild</h2></a>
+
+        <a name="reflektion"><h2>10. Reflektion</h2></a>
+        <ul>
+            <li>
+                <b>Herausforderungen und Lösung:</b>  <br> - responsive Design -> Lösung mittels Utitlity Classes, CSS Klassen war wichtige Grundlage für alles
+                <br> - Umgang mit Cookies und Sessions -> viel probiert, Hilfe via Internet, analysieren anderer Seiten
+                <br> - Teamarbeit und Zeitmanagment -> Verwendung von Jira, Discord Gruppe, regelmäßige Gruppentreffen
+                <br> - Datenbankmodell und Verwendung von SQL-Statements in PHP -> probieren, Datenbankmodelle von Onlineshops analysieren, DB Joins Produkttyp für Produktsuche kniffelig
+                <br> - Verwendung von AJAX -> viel Testen, Informationen über AJAX-Anwendung aus dem Internet
+                <br> - Produktsuche war etwas kniffelig -> viel Testen
+            </li>
+            <li>
+                <b>Besonderheiten für Nutzung ("Known-Bugs"):</b> <br> - wenig bis kein Fehlerhandling
+                <br> - Rollen werden nicht benutzt -> Keine Administratoroberfläche -> Man kann keine Produkte verwalten in der Oberfläche, nur über DB (spätere Implementierung möglich)
+                <br> - keine Kontoübersicht um eigene Daten (Passwort, Adresse, ...) zu ändern (spätere Implementierung möglich)
+                <br> - unbenutzte Tabellen in Datenbank("mobile" und "phone") -> könnte mit angepasstem Registrierungsformular implementiert werden
+                <br> - verstecke Zusatzfeatures: Showcase-Seite und 404-Seite (zu finden in Navbar -> Mehr -> Debug)
+                <br> - aus Zeitgründen häufige Nutzung von noscript -> mehrere Validierung mit Javascript jedoch implementierbar
+            </li>
+            <li>
+               <b>Projektmanagement-Dokumentation:</b>  <br>
+                <div style="overflow:auto; font-family:Arial">
+                  <?php include $_SERVER['DOCUMENT_ROOT'] . '/views/Jira.php';?>
+                </div>
+            </li>
+        </ul>
 
       </div>
     </div>
